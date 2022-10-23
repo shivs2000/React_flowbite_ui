200183992c15d4a29e7cb1361f1f0ed8ce9f1a41
SHIVAM@!! <43001208+shivs2000@users.noreply.github.com>
2022-10-23 20:21:06 +0530

    ititalizd and added flowbite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Rating, Card } from "flowbite-react";
+import FlowNav from "./components/FlowNav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FlowNav />
+      <div className="max-w-sm">
+        <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </Card>
+      </div>
+    </>
   );
 }
 
